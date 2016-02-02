839da85add0bb292a5624a018a4501fccda327f4
Dan Morrison <morrison.danr@gmail.com>
2016-02-01 21:42:47 -0500

    set up package.json and straighten out gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+// create new instance of BrowserSync
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function(gulpCallback) {
+  browserSync.init({
+    // serve out of ./
+    server: './',
+    open: true
+  }, function callback() {
+    // (server is now up)
+
+    // set up watch to reload browsers when source changes
+    gulp.watch(['./index.html', './js/*', './assets/*'], browserSync.reload);
+
+    // notify gulp that this task is done
+    gulpCallback();
+  });
+});
+
+/*
+DAN NOTE ON THIS GULPFILE
+I ended up using the documentation from http://paulsalaets.com/posts/injecting-styles-in-page-with-browser-sync/
+
+Basically, you just need to set up gulp serve as a task, and then plug in what you will watch
+
+If you need to access a deeper directory while working on it, change the start path property within the init object
+startPath: "./portfolio/lifeSciences.html",  --- like so
+*/
+
