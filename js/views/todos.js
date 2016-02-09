2f90fdbeac00c30582678d1b24ba4857fa436c47
Dan Morrison <morrison.danr@gmail.com>
2016-02-08 20:52:10 -0500

    create individual todo view models

---
+++ js/views/todos.js
@@ -0,0 +1,58 @@
+ var app = app || {};
+
+ //Todo Item View
+ // -------------
+
+app.TodoView = Backbone.View.extend({
+
+	// The DOM element for a todo item
+	tagName: 'li',
+
+	// Cache the template function for a single item
+	template: _.template( $('#item-template').html() ),
+
+	// Tje DOM events specific to an item.
+	events: {
+		'dblclick label': 'edit',
+		'keypress .edit': 'updateOnEnter',
+		'blur .edit': 'close'
+	},
+
+    // The TodoView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
+    // app, we set a direct reference on the model for convenience.
+	initialize: function() {
+		this.listenTo(this.model, 'change', this.render);
+	},
+
+    // Re-renders the titles of the todo item.
+	render: function() {
+		this.$el.html( this.template (this.mode.attributes) );
+		this.$input = this.$('.edit');
+		return this;
+	},
+
+	// Switch this view into "'editing'" mode, saving changes to the todo
+	edit: function() {
+		this.$el.addClass('editing');
+		this.$input.focus();
+	},
+
+	// Close the "'editing'" mode, saving changes to the todo
+	close: function() {
+		var value = this.$input.val().trim();
+
+		if ( value ) {
+			this.model.save({ title: value});
+		}
+
+		this.$el.removeClass('editing');
+	},
+
+	// If you hit "'enter'", we're through editing the item.
+	updateOnEnter: function ( e ) {
+		if ( e.which === ENTER_KEY ) {
+			this.close();
+		}
+	}
+});
